11bbcdca21a7a345dae37a5c619299a8a8a98f7d
hebermudezg <hebermudezg@unal.edu.co>
2020-11-21 15:37:15 -0500

    Buscar
    Se implementaron 2 busquedas

--- index.php
+++ index.php
@@ -34,55 +34,33 @@
 
                     <!-- Header -->
                     <div class="page-header clearfix">
-                        <h2 class="pull-left">Datalles de clientes32343423</h2>
+                        <h2 class="pull-left">Datalles de Clientes</h2>
                         <a href="create.php" class="btn btn-success pull-right">Agregar nuevo cliente</a>
                     </div>
 
 
-                     <!--esta conexion se hace con la base de datos para buscar por nombres o id-->
-                     <?php
-                        include_once 'config.php';
-                        $sentencia_select=$link->prepare('SELECT * FROM ferreamayadb ORDER BY id DESC');
-                        $sentencia_select->execute();
-                        $resultado=$sentencia_select->fetchAll();
+                    <form name="form" action="" method="post">
+                        <input type="text" name="subject" placeholder="Nombre y enter" >
+                        <a href="index.php" class="btn btn-success pull-right">Ver todo</a>
+                    </form>
 
-                        // metodo buscar
-                        if(isset($_POST['btn_buscar'])){
-                            $buscar_text=$_POST['buscar'];
-                            $select_buscar=$link->prepare(
-                                'SELECT * FROM ferreamayadb WHERE nombre LIKE :campo;'
-                            );
-
-                            $select_buscar->execute(array(
-                                ':campo' =>"%".$buscar_text."%"
-                            ));
-
-                            $resultado=$select_buscar->fetchAll();
-                        }
-                    ?>
-
-                    <!-- BUSCAR -->
-                    <div>
-                        <form action=""  method="post">
-                        <input type="text" name="buscar" placeholder="buscar nombre"
-                         value="<?php if(isset($buscar_text)) echo $buscar_text; ?>" >
-                        <input type="submit" class="btn" name="btn_buscar" value="Buscar nuevo cliente">
-                        </form>
-                    </div>
-
-
-
-
-
-
+                  
+                  
                     <!-- Query Mostrar -->
                     <?php
-                    // Include config file (para la conexion)
-                    require_once "config.php";
-                    
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM ferreamayadb";
 
+                  
+                    include_once 'config.php';
+
+                    if (!empty($_POST['subject'])){
+                        $buscar = $_POST['subject'];
+                    }else{
+                        $buscar = "";
+                    }
+
+
+
+                    $sql = "SELECT * FROM clientes WHERE nombre LIKE '%$buscar%'";
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
 
@@ -103,31 +81,6 @@
 
 
                                  //<!-- buscar -->
-                                foreach($resultado as $fila):?>
-                                    <tr>
-                                        <td><?php echo $fila['ID']; ?></td>
-                                        <td><?php echo $fila['Nombre']; ?></td>
-                                        <td><?php echo $fila['Apellido']; ?></td>
-                                        <td><?php echo $fila['Correo']; ?></td>
-                                        <td><?php echo $fila['FechaNac']; ?></td>
-                                        <td><a href="update.php?id=<?php echo $fila[$id]; ?>" class="btn__update"> Editar cliente</a></td>
-                                        <td><a href="delete.php?id=<?php echo $fila[$id]; ?>" class="btn__delete"> Eliminar cliente</a></td>
-                                    </tr>
-
-                                    echo "<tr>";
-                                        echo "<td>" . $row['id'] . "</td>";
-                                        echo "<td>" . $row['nombre'] . "</td>";
-                                        echo "<td>" . $row['dir'] . "</td>";
-                                        echo "<td>" . $row['tel'] . "</td>";
-                                        echo "<td>" . $row['email'] . "</td>";
-                                        echo "<td>" . $row['fecha_nac'] . "</td>";
-                                        echo "<td>";
-                                            echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                            echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                            echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-                                        echo "</td>";
-                                    echo "</tr>";
-                                endforeach
                         
                                 while($row = mysqli_fetch_array($result)){
                                     echo "<tr>";
@@ -149,8 +102,11 @@
                             echo "</table>";
                             // Free result set
                             mysqli_free_result($result);
-                        } else{
+                        } else {
                             echo "<p class='lead'><em>No records were found.</em></p>";
+
+
+
                         }
                     } else{
                         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
@@ -158,6 +114,9 @@
                     // Close connection *****************
                     mysqli_close($link);
                     ?>
+
+                    
+
                 </div>
             </div>        
         </div>
