ed7bb6eb604ca539b439bada37fff75828a96fc4
Jaime Andrés Molina <71019961+JaimeAndres-M@users.noreply.github.com>
2020-11-21 12:09:38 -0500

    cambiar idioma en algunos mensajes

--- delete.php
+++ delete.php
@@ -57,14 +57,14 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h1>Delete Record</h1>
+                        <h1>Eliminar registro</h1>
                     </div>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger fade in">
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this record?</p><br>
+                            <p>¿Está seguro de que quiere eliminar este registro?</p><br>
                             <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <input type="submit" value="Si" class="btn btn-danger">
                                 <a href="index.php" class="btn btn-default">No</a>
                             </p>
                         </div>
