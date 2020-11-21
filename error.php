c5bf64d6cfb13b6c7ac18dfe5e2650562d68fe46
Jaime Andrés Molina <71019961+JaimeAndres-M@users.noreply.github.com>
2020-11-21 11:37:43 -0500

    creacion del mensaje de sentencia invalida

---
+++ error.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 750px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Solicitud inválida</h1>
+                    </div>
+                    <div class="alert alert-danger fade in">
+                        <p> Lo siento, ha hecho una solicitud inválida. Por favor, <a href="index.php" class="alert-link">vuelve</a> e intentalo de nuevo.</p>
+                    </div>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
